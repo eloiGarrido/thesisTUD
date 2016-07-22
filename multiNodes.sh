f1ac8f521a601d3aa40c5179be74843d3978b8b4
eloiGarrido <garrido.eloi@gmail.com>
2016-07-22 14:24:24 +0200

    [Add multiNodes script]

---
+++ multiNodes.sh
@@ -0,0 +1,76 @@
+# #!/bin/sh
+
+cd
+rm /home/egarrido/staffetta_sensys2015/eh_staffetta/apps/glossy-test/staffetta-test.sky
+cp /media/egarrido/data/simulations/220716_multiNodes/orw/staffetta-test.sky /home/egarrido/staffetta_sensys2015/eh_staffetta/apps/glossy-test/
+cd contiki/tools/cooja/contiki_tests/
+bash RUN_REPEATED 5 /home/egarrido/mrmUNI_11_30min
+cd
+cp /home/egarrido/mrmUNI_11_30min-1.log /media/egarrido/data/simulations/220716_multiNodes/sim/mrmUniOrw_11_30min_1.log
+cp /home/egarrido/mrmUNI_11_30min-2.log /media/egarrido/data/simulations/220716_multiNodes/sim/mrmUniOrw_11_30min_2.log
+cp /home/egarrido/mrmUNI_11_30min-3.log /media/egarrido/data/simulations/220716_multiNodes/sim/mrmUniOrw_11_30min_3.log
+cp /home/egarrido/mrmUNI_11_30min-4.log /media/egarrido/data/simulations/220716_multiNodes/sim/mrmUniOrw_11_30min_4.log
+cp /home/egarrido/mrmUNI_11_30min-5.log /media/egarrido/data/simulations/220716_multiNodes/sim/mrmUniOrw_11_30min_5.log
+############################################################################################
+cd
+rm /home/egarrido/staffetta_sensys2015/eh_staffetta/apps/glossy-test/staffetta-test.sky
+cp /media/egarrido/data/simulations/220716_multiNodes/eh/staffetta-test.sky /home/egarrido/staffetta_sensys2015/eh_staffetta/apps/glossy-test/
+cd contiki/tools/cooja/contiki_tests/
+bash RUN_REPEATED 5 /home/egarrido/mrmUNI_11_30min
+cd
+cp /home/egarrido/mrmUNI_11_30min-1.log /media/egarrido/data/simulations/220716_multiNodes/sim/mrmUniEh_11_30min_1.log
+cp /home/egarrido/mrmUNI_11_30min-2.log /media/egarrido/data/simulations/220716_multiNodes/sim/mrmUniEh_11_30min_2.log
+cp /home/egarrido/mrmUNI_11_30min-3.log /media/egarrido/data/simulations/220716_multiNodes/sim/mrmUniEh_11_30min_3.log
+cp /home/egarrido/mrmUNI_11_30min-4.log /media/egarrido/data/simulations/220716_multiNodes/sim/mrmUniEh_11_30min_4.log
+cp /home/egarrido/mrmUNI_11_30min-5.log /media/egarrido/data/simulations/220716_multiNodes/sim/mrmUniEh_11_30min_5.log
+############################################################################################
+cd
+rm /home/egarrido/staffetta_sensys2015/eh_staffetta/apps/glossy-test/staffetta-test.sky
+cp /media/egarrido/data/simulations/220716_multiNodes/eh_dc/staffetta-test.sky /home/egarrido/staffetta_sensys2015/eh_staffetta/apps/glossy-test/
+cd contiki/tools/cooja/contiki_tests/
+bash RUN_REPEATED 5 /home/egarrido/mrmUNI_11_30min
+cd
+cp /home/egarrido/mrmUNI_11_30min-1.log /media/egarrido/data/simulations/220716_multiNodes/sim/mrmUniEhDc_11_30min_1.log
+cp /home/egarrido/mrmUNI_11_30min-2.log /media/egarrido/data/simulations/220716_multiNodes/sim/mrmUniEhDc_11_30min_2.log
+cp /home/egarrido/mrmUNI_11_30min-3.log /media/egarrido/data/simulations/220716_multiNodes/sim/mrmUniEhDc_11_30min_3.log
+cp /home/egarrido/mrmUNI_11_30min-4.log /media/egarrido/data/simulations/220716_multiNodes/sim/mrmUniEhDc_11_30min_4.log
+cp /home/egarrido/mrmUNI_11_30min-5.log /media/egarrido/data/simulations/220716_multiNodes/sim/mrmUniEhDc_11_30min_5.log
+############################################################################################
+############################################################################################
+cd
+rm /home/egarrido/staffetta_sensys2015/eh_staffetta/apps/glossy-test/staffetta-test.sky
+cp /media/egarrido/data/simulations/220716_multiNodes/orw/staffetta-test.sky /home/egarrido/staffetta_sensys2015/eh_staffetta/apps/glossy-test/
+cd contiki/tools/cooja/contiki_tests/
+bash RUN_REPEATED 5 /home/egarrido/mrmUNI_21_30min
+cd
+cp /home/egarrido/mrmUNI_21_30min-1.log /media/egarrido/data/simulations/220716_multiNodes/sim/mrmUniOrw_21_30min_1.log
+cp /home/egarrido/mrmUNI_21_30min-2.log /media/egarrido/data/simulations/220716_multiNodes/sim/mrmUniOrw_21_30min_2.log
+cp /home/egarrido/mrmUNI_21_30min-3.log /media/egarrido/data/simulations/220716_multiNodes/sim/mrmUniOrw_21_30min_3.log
+cp /home/egarrido/mrmUNI_21_30min-4.log /media/egarrido/data/simulations/220716_multiNodes/sim/mrmUniOrw_21_30min_4.log
+cp /home/egarrido/mrmUNI_21_30min-5.log /media/egarrido/data/simulations/220716_multiNodes/sim/mrmUniOrw_21_30min_5.log
+############################################################################################
+cd
+rm /home/egarrido/staffetta_sensys2015/eh_staffetta/apps/glossy-test/staffetta-test.sky
+cp /media/egarrido/data/simulations/220716_multiNodes/eh/staffetta-test.sky /home/egarrido/staffetta_sensys2015/eh_staffetta/apps/glossy-test/
+cd contiki/tools/cooja/contiki_tests/
+bash RUN_REPEATED 5 /home/egarrido/mrmUNI_21_30min
+cd
+cp /home/egarrido/mrmUNI_21_30min-1.log /media/egarrido/data/simulations/220716_multiNodes/sim/mrmUniEh_21_30min_1.log
+cp /home/egarrido/mrmUNI_21_30min-2.log /media/egarrido/data/simulations/220716_multiNodes/sim/mrmUniEh_21_30min_2.log
+cp /home/egarrido/mrmUNI_21_30min-3.log /media/egarrido/data/simulations/220716_multiNodes/sim/mrmUniEh_21_30min_3.log
+cp /home/egarrido/mrmUNI_21_30min-4.log /media/egarrido/data/simulations/220716_multiNodes/sim/mrmUniEh_21_30min_4.log
+cp /home/egarrido/mrmUNI_21_30min-5.log /media/egarrido/data/simulations/220716_multiNodes/sim/mrmUniEh_21_30min_5.log
+############################################################################################
+cd
+rm /home/egarrido/staffetta_sensys2015/eh_staffetta/apps/glossy-test/staffetta-test.sky
+cp /media/egarrido/data/simulations/220716_multiNodes/eh_dc/staffetta-test.sky /home/egarrido/staffetta_sensys2015/eh_staffetta/apps/glossy-test/
+cd contiki/tools/cooja/contiki_tests/
+bash RUN_REPEATED 5 /home/egarrido/mrmUNI_21_30min
+cd
+cp /home/egarrido/mrmUNI_21_30min-1.log /media/egarrido/data/simulations/220716_multiNodes/sim/mrmUniEhDc_21_30min_1.log
+cp /home/egarrido/mrmUNI_21_30min-2.log /media/egarrido/data/simulations/220716_multiNodes/sim/mrmUniEhDc_21_30min_2.log
+cp /home/egarrido/mrmUNI_21_30min-3.log /media/egarrido/data/simulations/220716_multiNodes/sim/mrmUniEhDc_21_30min_3.log
+cp /home/egarrido/mrmUNI_21_30min-4.log /media/egarrido/data/simulations/220716_multiNodes/sim/mrmUniEhDc_21_30min_4.log
+cp /home/egarrido/mrmUNI_21_30min-5.log /media/egarrido/data/simulations/220716_multiNodes/sim/mrmUniEhDc_21_30min_5.log
+############################################################################################
+############################################################################################
